769f1cbf2a99f328a053db9ae96ef2e94184fd06
figo5530 <figooooo0221@gmail.com>
2021-08-11 18:09:49 +0800

    adds logo and navbar and some styling to the them with Menu from antd

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,6 +1,15 @@
 import React from 'react' 
+import { Menu } from 'antd'
 
+const MenuItem = Menu.Item
 const header = () => {
-    return <div>Header</div>
+    return <div className="header">
+        <img className="logo" src={require('@/assets/logo.png')} alt="logo" />
+        <Menu className="menus" mode="horizontal" theme="light">
+            <MenuItem>
+                Home
+            </MenuItem>
+        </Menu>
+    </div>
 }
 export default header
