9994b62aaeb1656e24be17a7b8faa50384db85e3
figo5530 <figooooo0221@gmail.com>
2021-08-12 11:42:25 +0800

    adds withRouter in header for MenuItem to link to the right path.

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,15 +1,17 @@
 import React from 'react' 
 import { Menu } from 'antd'
+import Link from 'umi/link'
+import withRouter from 'umi/withRouter'
 
 const MenuItem = Menu.Item
-const header = () => {
+const header = ({ location }) => {
     return <div className="header">
         <img className="logo" src={require('@/assets/logo.png')} alt="logo" />
-        <Menu className="menus" mode="horizontal" theme="light">
-            <MenuItem>
-                Home
+        <Menu className="menus" mode="horizontal" theme="light" selectedKeys={[location.pathname]}>
+            <MenuItem key='/'>
+            <Link to="/">Home</Link>
             </MenuItem>
         </Menu>
     </div>
 }
-export default header
+export default withRouter(header)
