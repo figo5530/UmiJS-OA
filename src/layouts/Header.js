53692d4e4c168da71d3494a51d290f5b3fd85b0e
figo5530 <figooooo0221@gmail.com>
2021-08-13 13:54:43 +0800

    adds top right icon admin and sign out dropdown meu.

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,17 @@
 import React from 'react' 
-import { Menu } from 'antd'
+import { Menu, Dropdown, Icon } from 'antd'
 import Link from 'umi/link'
 import withRouter from 'umi/withRouter'
 
 const MenuItem = Menu.Item
 const header = ({ location }) => {
+    const menu = (
+        <Menu>
+            <MenuItem>
+                <span>Sign Out</span>
+            </MenuItem>
+        </Menu>
+    )
     return <div className="header">
         <img className="logo" src={require('@/assets/logo.png')} alt="logo" />
         <Menu className="menus" mode="horizontal" theme="light" selectedKeys={[location.pathname]}>
@@ -12,6 +19,13 @@ const header = ({ location }) => {
             <Link to="/">Home</Link>
             </MenuItem>
         </Menu>
+        <div className='right'>
+            <Dropdown overlay={menu} >
+                <a className="ant-dropdown-link" href="www.google.com">
+                    <Icon type='user' style={{ marginRight: 3 }}/> admin
+                </a>
+            </Dropdown>
+        </div>
     </div>
 }
 export default withRouter(header)
