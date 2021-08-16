a5e4f64d727be3b7042d4a1ed385b8313f274d35
figo5530 <figooooo0221@gmail.com>
2021-08-16 10:47:46 +0800

    adds styling to the login page with footer

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Icon } from 'antd'
+import styles from './index.scss'
 
-const { Content } = Layout;
+const { Content, Footer } = Layout;
 export default function index() {
     return (
         <Layout>
-                <Content>Login</Content>
+            <Content className={styles.content}>Login</Content>
+            <Footer className={styles.footer}>
+                Copyright <Icon type="copyright" /> 2021 Yinghui Gan
+            </Footer>
         </Layout>
     )
     
