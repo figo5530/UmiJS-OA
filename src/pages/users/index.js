92c4207924220855d8e40fa545eb3c71cde153cd
figo5530 <figooooo0221@gmail.com>
2021-08-21 17:44:54 +0800

    gives user page a basic layout and some content

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,9 +1,15 @@
+/**
+ * title: User
+ */
 import React from 'react'
-
+import  { Button } from 'antd'
+import styles from './index.scss'
 const index = () => {
     return (
-        <div>
-            USERS
+        <div className={`${styles['content-wrapper']}`}>
+            <div className={`${styles['tool-wrapper']}`}>
+                <Button type="primary">Add User</Button>
+            </div>
         </div>
     )
 }
