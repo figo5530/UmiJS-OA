87bf00566a5b2eeb17ed28f07cc299be0f670e19
figo5530 <figooooo0221@gmail.com>
2021-08-23 14:43:25 +0800

    adds columns in user page

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -2,14 +2,45 @@
  * title: User
  */
 import React from 'react'
-import  { Button } from 'antd'
+import  { Button, Table } from 'antd'
 import styles from './index.scss'
 const index = () => {
+    const columns =[
+        {
+            title: 'Username',
+            dataIndex: 'username',
+            key: 'username',
+            width: '25%'
+        },
+        {
+            title: 'Nickname',
+            dataIndex: 'nickname',
+            key: 'nickname',
+            width: '25%'
+        },
+        {
+            title: 'Type',
+            dataIndex: 'type',
+            key: 'type',
+            width: '25%'
+        },
+        {
+            title: 'Operation',
+            key: 'operation',
+            render: (text, record) => (
+                <div>
+                    <a>Edit</a>
+                    <a>Delete</a>
+                </div>
+            )
+        },
+    ]
     return (
         <div className={`${styles['content-wrapper']}`}>
             <div className={`${styles['tool-wrapper']}`}>
                 <Button type="primary">Add User</Button>
             </div>
+            <Table columns={columns}/>
         </div>
     )
 }
