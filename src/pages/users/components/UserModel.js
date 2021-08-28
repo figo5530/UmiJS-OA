050395c11b35ed0e6b1024e97bd1f916d77e3b6e
figo5530 <figooooo0221@gmail.com>
2021-08-28 15:48:04 +0800

    implements the form of modal

--- src/pages/users/components/UserModel.js
+++ src/pages/users/components/UserModel.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
-import { Modal } from 'antd'
+import { Modal, Form, Input, Radio } from 'antd'
+
+const FormItem = Form.Item
+const RadioGroup = Radio.Group
+
 class UserModel extends Component {
     render() {
         return (
             <Modal title="Add User" visible={true} >
-                Prompt window
+                <Form>
+                    <FormItem label="username">
+                        <Input placeholder="Please input the username"/>
+                    </FormItem>
+                    <FormItem label="name">
+                        <Input placeholder="Please input the name"/>
+                    </FormItem>
+                    <FormItem label="type">
+                        <RadioGroup>
+                            <Radio value={'0'}>Adminstrator</Radio>
+                            <Radio value={'1'}>User</Radio>
+                        </RadioGroup>
+                    </FormItem>
+                </Form>
             </Modal>
         )
     }
