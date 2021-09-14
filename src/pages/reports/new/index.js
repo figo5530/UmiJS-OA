04b53132b44ba6f74d56608181083ec469297ddb
figo5530 <figooooo0221@gmail.com>
2021-09-14 17:12:13 +0800

    adds a content in report/new component

---
+++ src/pages/reports/new/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Form, Input, Select, Button } from 'antd'
+import { Content } from '@/components/Layout'
+
+
+class index extends Component {
+    render() {
+        return (
+            <Content>
+                
+            </Content>
+        )
+    }
+}
+
+export default index
