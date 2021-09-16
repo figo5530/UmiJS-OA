e59058430547f381497133f59abe4e8b6057a595
figo5530 <figooooo0221@gmail.com>
2021-09-16 14:11:34 +0800

    adds editor validaion and warning style

--- src/pages/reports/write/index.js
+++ src/pages/reports/write/index.js
@@ -10,17 +10,37 @@ import E from 'wangeditor'
 
 class index extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            editorContent: null,
+            editorCheck: true
+        }
+    }
+
     componentDidMount() {
         this.initEditor()
     }
 
     initEditor() {
         const editor = new E(this.refs.editorRef)
+
+        editor.customConfig.onchage = html => {
+            let editorCheck = true
+            if (!html || html == "<p><br/></p>") {
+                editorCheck = false
+            }
+            this.setState({
+                editorContent: html,
+                editorCheck: editorCheck
+            })
+        }
         editor.create()
     }
 
     render() {
         const { getFieldDecorator } = this.props.form
+        const { editorCheck } = this.state
         return (
             <Content>
                 <Form>
@@ -47,7 +67,8 @@ class index extends Component {
                         <Select placeholder="Please select the recipient"/>)}
                     </Form.Item>
                     <Form.Item label="Content" required>
-                        <div ref="editorRef"/>
+                        <div ref="editorRef" style={!editorCheck ? {border: '1px red solid'} : {border: '1px #eee solid'}}/>
+                        {!editorCheck && <p style={{color: 'red'}}>Content can not be empty</p>}
                     </Form.Item>
                     <Form.Item className="action">
                         <Button>Cancel</Button>
